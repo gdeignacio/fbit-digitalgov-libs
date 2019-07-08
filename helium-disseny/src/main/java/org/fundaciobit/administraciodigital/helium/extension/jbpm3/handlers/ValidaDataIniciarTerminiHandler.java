d5bebcf22fea746f1a84a1c4a06b3cf2292b0b82
gdeignacio-fundaciobit <gdeignacio@fundaciobit.org>
2019-07-08 17:34:51 +0200

    Handlers

---
+++ helium-disseny/src/main/java/org/fundaciobit/administraciodigital/helium/extension/jbpm3/handlers/ValidaDataIniciarTerminiHandler.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2017 fundaciobit
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fundaciobit.administraciodigital.helium.extension.jbpm3.handlers;
+
+import java.util.Date;
+import net.conselldemallorca.helium.jbpm3.handlers.TerminiIniciarHandler;
+import org.jbpm.graph.exe.ExecutionContext;
+
+/**
+ *
+ * @author josepg-limit
+ */
+
+@SuppressWarnings("serial")
+public class ValidaDataIniciarTerminiHandler extends TerminiIniciarHandler {
+
+    private String varData;
+    private String autoLeave;
+
+    /**
+     * Ejecuta las operaciones del handler.
+     */
+    public void execute(ExecutionContext context) throws Exception {
+
+        if (varData != null && varData.length() > 0) {
+            Object obj = context.getVariable(varData);
+            if (obj != null && obj instanceof Date) {
+                cridaSuperHandler(context);
+            }
+        }
+       
+        if ("true".equals(autoLeave)) context.leaveNode();
+        
+    }
+
+    private void cridaSuperHandler(ExecutionContext context) throws Exception {
+        super.setVarData(varData);
+        super.execute(context);
+    }
+
+    public void setVarData(String varData) {
+        this.varData = varData;
+    }
+    
+    public void setAutoLeave(String autoLeave){
+        this.autoLeave = autoLeave;
+    }
+    
+}
