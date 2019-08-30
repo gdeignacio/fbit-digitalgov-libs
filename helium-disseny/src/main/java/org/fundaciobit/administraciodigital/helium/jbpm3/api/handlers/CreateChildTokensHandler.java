02b51d75148b376f0c38ff9f8ca500ba2400ef25
gdeignacio-fundaciobit <gdeignacio@fundaciobit.org>
2019-08-30 16:02:52 +0200

    Token Handler

--- helium-disseny/src/main/java/org/fundaciobit/administraciodigital/helium/jbpm3/api/handlers/CreateChildTokensHandler.java
+++ helium-disseny/src/main/java/org/fundaciobit/administraciodigital/helium/jbpm3/api/handlers/CreateChildTokensHandler.java
@@ -1,7 +1,5 @@
 package org.fundaciobit.administraciodigital.helium.jbpm3.api.handlers;
 
-import org.fundaciobit.administraciodigital.helium.jbpm3.handlers.*;
-import java.util.Arrays;
 import java.util.List;
 import net.conselldemallorca.helium.jbpm3.api.HeliumActionHandler;
 import net.conselldemallorca.helium.jbpm3.api.HeliumApi;
@@ -10,11 +8,9 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jbpm.JbpmConfiguration;
 import org.jbpm.JbpmContext;
-import org.jbpm.graph.exe.ExecutionContext;
-import org.jbpm.graph.exe.ProcessInstance;
 import org.jbpm.graph.exe.Token;
-//import org.fundaciobit.administraciodigital.dir3caib.rest.client.CatalogoDIR3Client;
 import org.fundaciobit.administraciodigital.helium.jbpm3.utils.Dir3Client;
+import org.json.simple.JSONObject;
 
 /**
  *
@@ -28,15 +24,6 @@ public class CreateChildTokensHandler extends HeliumActionHandler {
     String varChildren;
     String varChild;
     String multiple;
-    
-    
-    //private final static String NO_DIR3 = "NDIR3";
-
-    //String nodeDesti;
-    //String varFilla;
-    //String varFillaDesc;
-    //String varMultiple;
-    //String esMultiple;
 
     private static final long serialVersionUID = 3513847803530877133L;
 
@@ -48,84 +35,31 @@ public class CreateChildTokensHandler extends HeliumActionHandler {
     @Override
     public void execute(HeliumApi api) throws HeliumHandlerException {
         
-        
-        
-        
-        
-
         JbpmContext context = JbpmConfiguration.getInstance().createJbpmContext();
-
-        ExecutionContext executionContext = new ExecutionContext(context.getToken(api.getToken().getId()));
-
-        ProcessInstance processInstance = context.getToken(api.getToken().getId()).getProcessInstance();
-
-        Object obj = executionContext.getVariable(varMultiple);
-        boolean multiple = "true".equalsIgnoreCase(esMultiple);
+        Token mainToken = context.getToken(api.getToken().getId());
         
-        if (obj == null) {
-            return;
-        }
+        //ExecutionContext executionContext = new ExecutionContext(mainToken);
+        //ProcessInstance processInstance = mainToken.getProcessInstance();
         
-        Object[] tokenSuffixes = (multiple && obj instanceof Object[]) ? (Object[]) obj : new Object[]{obj};
-
+        Object obj = api.getVariable(varChildren);
+        boolean esMultiple = "true".equalsIgnoreCase(multiple);
+        if (obj == null) {return;}
+        Object[] tokenSuffixes = (esMultiple && obj instanceof Object[]) ? (Object[]) obj : new Object[]{obj};
+        String tokenName = null;
         for (Object tokenSuffix : tokenSuffixes) {
-
-            Object[] aTokenSuffix = null;
-            String sTokenSuffix = null;
-            boolean isArray;
-            String lastItem = null;
-            String lastButOneItem = null;
-            if (tokenSuffix instanceof Object[]){
-                aTokenSuffix = (Object[]) tokenSuffix;
-                isArray = true;
-                lastItem = aTokenSuffix[aTokenSuffix.length -1].toString();
-                if (aTokenSuffix.length>1){   
-                    lastButOneItem = aTokenSuffix[aTokenSuffix.length -2].toString();
-                    String lastItemNoDir3 = NO_DIR3.concat(lastItem);
-                    lastItem = (NO_DIR3.equals(lastButOneItem))?lastItemNoDir3:lastButOneItem;
-                }    
-            } else {
-                sTokenSuffix = tokenSuffix.toString();
-                isArray = false;
-                lastItem = sTokenSuffix;
-            }
-            
-            String strTokenSuffix = (isArray)?Arrays.toString(aTokenSuffix).replace(", ", "#").replaceAll("[\\[\\]]", ""):sTokenSuffix;
-            
-            //String.join("_", (List)Arrays.asList((Object[])tokenSuffix) )
-            //String strTokenSuffix = (tokenSuffix instanceof Object[]) ? Arrays.toString((Object[]) tokenSuffix).replace(", ", "#").replaceAll("[\\[\\]]", "") : tokenSuffix.toString();
-            
-            Token tokenPare = executionContext.getToken();
-            String tokenName = getTokenName(tokenPare, nodeDesti + "_" + strTokenSuffix);
-            Token tokenFill = new Token(tokenPare, tokenName);
-            executionContext.setVariable(varFilla, tokenSuffix);
-            
-            log.info("Last item abans: "  + lastItem);
-            
-            String description = fillDescription(lastItem);
-            
-            log.info("Last item: "  + lastItem);
-            log.info("Descripcion: "  + description);
-            
-            executionContext.setVariable(varFillaDesc, description);
-            printInfo(tokenName, nodeDesti, varFilla, tokenSuffix);
-            
-            //tokenRedirigir(tokenFill.getId(), nodeDesti, false);
-
-            api.expedientTokenRedirigir(tokenFill.getId(), nodeDesti, multiple);
-            
+            tokenName=destinationNode;
+            tokenName+="_";
+            JSONObject json = new JSONObject();
+            json.put(varChild, tokenSuffix);
+            tokenName+=json.toJSONString();
+            Token token = new Token(mainToken, tokenName);
+            printInfo(tokenName, destinationNode, varChild, tokenSuffix);
+            api.expedientTokenRedirigir(token.getId(), destinationNode, false);
         }
+      
     }
     
-    /*
-    private String fillDescription(String key){
-        CatalogoDIR3Client client = CatalogoDIR3Client.getClient();
-        String denominacion = client.getByCodigo(key);
-        return denominacion;
-    }
-    */
-
-    
+   
     private String fillDescription(String key) {
         Dir3Client client = Dir3Client.getClient();
         log.info("Denominacion[" + key + "]: ");
@@ -134,28 +68,7 @@ public class CreateChildTokensHandler extends HeliumActionHandler {
         return denominacion;
     }
     
-    
-    //public void setNodeDesti(String nodeDesti) {
-    //    this.nodeDesti = nodeDesti;
-    // }
-
-    //public void setVarFilla(String varFilla) {
-    //    this.varFilla = varFilla;
-    //}
-    
-    // public void setVarFillaDesc(String varFillaDesc) {
-    //    this.varFillaDesc = varFillaDesc;
-    //}
-
-    //public void setVarMultiple(String varMultiple) {
-    //    this.varMultiple = varMultiple;
-    //}
-
-    //public void setEsMultiple(String esMultiple) {
-    //    this.esMultiple = esMultiple;
-    //}
-
-    //protected abstract String getTokenSuffixElement(Object o);
+   
     private void printInfo(String tokenName, String destinationNode, String child, Object valor) {
 
         String s = ">>> Redirigint token fill (tokenName=" + tokenName + ", "
